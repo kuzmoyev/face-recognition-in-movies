a3a0ac6ad9f0ea65547dd2f941872f2edfdcb359
kuzmoyev <kuzmovich.goog@gmail.com>
2018-04-29 19:07:54 +0200

    Add additional-photos parameter. Update logger to work with progress bar

--- src/movie_face_recognition.py
+++ src/movie_face_recognition.py
@@ -18,34 +18,62 @@ def parse_args():
     parser.add_argument('-v', '--verbose', help='detailed verbose output', required=False, action='store_true')
     parser.add_argument('-s', '--show', help='show output frames', required=False, action='store_true')
     parser.add_argument('-f', '--faces', help='highlight found faces', required=False, action='store_true')
+    parser.add_argument('-a', '--additional-photos', help='additional photos',
+                        required=False, nargs=argparse.ONE_OR_MORE, action='append', metavar=('NAME PATH', 'PATH'))
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if any(len(l) < 2 for l in args.additional_photos):
+        print('Parameter -a/--additional-photos requires at least 2 arguments.')
+        print('usage: movie_face_recognition.py FILE_NAME MOVIE_TITLE -a NAME PATH [PATH ...]')
+        exit()
+
+    return args
 
 
 def init_logger(verbose):
     VERBOSE_LEVEL = 15
 
-    def _verbose(self, message, *args, **kws):
+    def _verbose(self, message, pbar=None, *args, **kwargs):
         if self.isEnabledFor(VERBOSE_LEVEL):
-            self._log(VERBOSE_LEVEL, message, args, **kws)
+            if pbar:
+                pbar.write(message)
+            else:
+                self._log(VERBOSE_LEVEL, message, args, **kwargs)
+
+    def _info(self, message, pbar=None, *args, **kwargs):
+        if self.isEnabledFor(logging.INFO):
+            if pbar:
+                pbar.write(message)
+            else:
+                self._log(logging.INFO, message, args, **kwargs)
 
     logging.Logger.v = _verbose
-    logging.Logger.i = logging.Logger.info
+    logging.Logger.i = _info
 
     logging.basicConfig(format='%(message)s', datefmt='%H:%M:%S', level=VERBOSE_LEVEL if verbose else logging.INFO)
 
 
-if __name__ == '__main__':
+def main():
     args = parse_args()
     init_logger(args.verbose)
 
-    input_video = args.FILE_NAME
+    input_video = args.FILE_NAME if args.FILE_NAME != '0' else 0
     output_video = args.output
     movie_title = args.MOVIE_TITLE
     show_frames = args.show
     highlight_faces = args.faces
 
+    print(args)
+
     cast = imdb.get_cast_images(movie_title)
+
+    for name, *paths in args.additional_photos:
+        cast[name] = paths
 
     face_recognizer = FaceRecognizer(show=show_frames, highlight_faces=highlight_faces)
     face_recognizer.process(input_video, cast, output_video)
+
+
+if __name__ == '__main__':
+    main()

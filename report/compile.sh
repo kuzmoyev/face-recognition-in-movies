fd05c6c8a68ed40cb7dd40fa286df47ad612de80
kuzmoyev <kuzmovich.goog@gmail.com>
2018-04-30 15:52:21 +0200

    Init report

---
+++ report/compile.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+OUT_DIR=pdf
+TEX_FILE=$(echo *.tex)
+AUX_FILE=${OUT_DIR}/${TEX_FILE/tex/aux}
+PDF_FILE=${TEX_FILE/tex/pdf}
+LOG_FILE=${OUT_DIR}/compilation.log
+
+mkdir -p ${OUT_DIR}
+pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -output-directory=${OUT_DIR} \
+         ${TEX_FILE} > ${LOG_FILE} && \
+bibtex -terse -min-crossrefs=1 ${AUX_FILE} && \
+pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -output-directory=${OUT_DIR} \
+         ${TEX_FILE} > ${LOG_FILE} && \
+pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -output-directory=${OUT_DIR} \
+         ${TEX_FILE} > ${LOG_FILE}
+
+
+mv ${OUT_DIR}/${PDF_FILE} ${PDF_FILE}
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}"
+    echo -n "Successfully Compiled ${PDF_FILE}"
+    xdg-open ${PDF_FILE}
+else
+    head -n -3 ${LOG_FILE} | sed "s/^\..*\/\([^\/]*\.tex:[0-9]*\):/\.(\1)/"
+    echo -e "${RED}"
+    echo -n "Compilation Error ${TEX_FILE}"
+fi
+echo -e "${NC}"

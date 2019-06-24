2de91df19fd1703f00b8a42c5adb65b797f9e8cd
kuzmoyev <kuzmovich.goog@gmail.com>
2019-06-24 16:49:18 +0200

    Add nn topology

--- report/compile.sh
+++ report/compile.sh
@@ -9,6 +9,7 @@ AUX_FILE=${OUT_DIR}/${TEX_FILE/tex/aux}
 PDF_FILE=${TEX_FILE/tex/pdf}
 LOG_FILE=${OUT_DIR}/compilation.log
 
+
 mkdir -p ${OUT_DIR}
 pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -output-directory=${OUT_DIR} \
          ${TEX_FILE} > ${LOG_FILE} && \
@@ -24,9 +25,9 @@ if [ $? -eq 0 ]; then
     mv ${OUT_DIR}/${PDF_FILE} ${PDF_FILE}
     echo -e "${GREEN}"
     echo -n "Successfully Compiled ${PDF_FILE}"
-    xdg-open ${PDF_FILE}
+    open ${PDF_FILE}
 else
-    head -n -3 ${LOG_FILE} | sed "s/^\..*\/\([^\/]*\.tex:[0-9]*\):/\.(\1)/"
+    cat ${LOG_FILE} | sed "s/^\..*\/\([^\/]*\.tex:[0-9]*\):/\.(\1)/"
     echo -e "${RED}"
     echo -n "Compilation Error ${TEX_FILE}"
 fi
